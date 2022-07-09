f76356282e1ed9fbc226f14734e095b62964aadd
Akshay krisha <akshay@bpract.com>
2022-07-09 19:20:56 +0530

    Changed the Header/index.js to take in a new field that is added to yourData.json named "hideBlog". and hid the blog values from navigation.

--- yourData.js
+++ yourData.js
@@ -120,6 +120,9 @@ export default {
   aboutpara:
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   //   End About Section ---------------------
+
+  // Hide Blog section
+  hideBlog: true,
 };
 
 // Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
