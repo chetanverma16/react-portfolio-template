26d918bf2d5217d58db5eac5adfc74daee8f8554
Chetan Verma <chetanverma@Chetans-MacBook-Pro.local>
2022-07-27 16:13:31 +0530

    Patch Fix

--- components/ServiceCard/index.js
+++ components/ServiceCard/index.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 
 const ServiceCard = ({ name, description }) => {
   const { theme } = useTheme();
+  const [mounted, setMounted] = useState();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
   return (
     <div
       className={`w-full p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
-        theme === "dark" ? "hover:bg-slate-800" : "hover:bg-slate-50"
+        mounted && theme === "dark" ? "hover:bg-slate-800" : "hover:bg-slate-50"
       } hover:scale-105 cursor-pointer`}
     >
       <h1 className="text-3xl">{name ? name : "Heading"}</h1>
