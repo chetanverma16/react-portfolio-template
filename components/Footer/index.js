480a8cf21ea12ffb06481446b6ecaf1352b0514b
Chetan Verma <chetan61verma@gmail.com>
2022-08-18 21:29:18 +0530

    Adding new footer design

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Socials from "../Socials";
 import Link from "next/link";
+import Button from "../Button";
 
 const Footer = ({ dark }) => {
   return (
     <>
       <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
-        <h1 className="text-2xl text-bold">Contact.</h1>
-        <div className="mt-5">
-          <Socials />
+        <div>
+          <h1 className="text-2xl text-bold">Contact.</h1>
+          <div className="mt-10">
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              LET'S WORK
+            </h1>
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              TOGETHER
+            </h1>
+            <Button type="primary">Schedule a call</Button>
+            <div className="mt-10">
+              <Socials />
+            </div>
+          </div>
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
