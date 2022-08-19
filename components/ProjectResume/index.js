0ac6fdf271b65b843ccaaf01e82be4c254bdf47a
Chetan Verma <chetan61verma@gmail.com>
2022-08-20 00:06:35 +0530

    Adding Resume Edit

--- components/ProjectResume/index.js
+++ components/ProjectResume/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 const ProjectResume = ({ dates, type, position, bullets }) => {
+  const [bulletsLocal, setBulletsLocal] = React.useState(bullets.split(","));
+
   return (
-    <div className="mt-5 flex mob:flex-col desktop:flex-row justify-between">
-      <div className="text-lg">
+    <div className="mt-5 w-full flex mob:flex-col desktop:flex-row justify-between">
+      <div className="text-lg w-2/5">
         <h2>{dates}</h2>
         <h3 className="text-sm opacity-50">{type}</h3>
       </div>
-      <div>
+      <div className="w-3/5">
         <h2 className="text-lg font-bold">{position}</h2>
-        {bullets && bullets.length > 0 && (
+        {bulletsLocal && bulletsLocal.length > 0 && (
           <ul className="list-disc">
-            {bullets.map((bullet, index) => (
+            {bulletsLocal.map((bullet, index) => (
               <li key={index} className="text-sm my-1 opacity-70">
                 {bullet}
               </li>
