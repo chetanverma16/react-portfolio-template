0b45e59f98db49e8d077f9c6e4b117c6f60b350c
Sanjay <102804548+sanjayk0508@users.noreply.github.com>
2022-09-11 13:43:01 +0530

    Adjusted the "Edit Button" properly

--- pages/index.js
+++ pages/index.js
@@ -54,14 +54,7 @@ export default function Home() {
       <Head>
         <title>{data.name}</title>
       </Head>
-      {/* This button should not go into production */}
-      {process.env.NODE_ENV === "development" && (
-        <div className="fixed bottom-5 right-5">
-          <Link href="/edit">
-            <Button type="primary">Edit Data</Button>
-          </Link>
-        </div>
-      )}
+
       <div className="gradient-circle"></div>
       <div className="gradient-circle-bottom"></div>
 
@@ -102,6 +95,7 @@ export default function Home() {
         </div>
         <div className="mt-10 laptop:mt-30 p-2 laptop:p-0" ref={workRef}>
           <h1 className="text-2xl text-bold">Work.</h1>
+
           <div className="mt-5 laptop:mt-10 grid grid-cols-1 tablet:grid-cols-2 gap-4">
             {data.projects.map((project) => (
               <WorkCard
@@ -114,6 +108,7 @@ export default function Home() {
             ))}
           </div>
         </div>
+
         <div className="mt-10 laptop:mt-30 p-2 laptop:p-0">
           <h1 className="tablet:m-10 text-2xl text-bold">Services.</h1>
           <div className="mt-5 tablet:m-10 grid grid-cols-1 laptop:grid-cols-2 gap-6">
@@ -126,6 +121,14 @@ export default function Home() {
             ))}
           </div>
         </div>
+        {/* This button should not go into production */}
+        {process.env.NODE_ENV === "development" && (
+          <div className="fixed bottom-5 right-5">
+            <Link href="/edit">
+              <Button type="primary">Edit Data</Button>
+            </Link>
+          </div>
+        )}
         <div className="mt-10 laptop:mt-40 p-2 laptop:p-0" ref={aboutRef}>
           <h1 className="tablet:m-10 text-2xl text-bold">About.</h1>
           <p className="tablet:m-10 mt-2 text-xl laptop:text-3xl w-full laptop:w-3/5">
