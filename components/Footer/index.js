0802968e99a9f0c0905adaa28d8fcce4db4ea509
samuelajalode <113217825+samuelajalode@users.noreply.github.com>
2023-01-05 17:11:18 +0100

    first commPortfolio

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -26,7 +26,7 @@ const Footer = ({}) => {
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
         <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
+          <a className="underline underline-offset-1">SamuelYoung</a>
         </Link>
       </h1>
     </>
