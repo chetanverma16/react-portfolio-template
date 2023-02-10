7a5b294ef8d0fbe384aed19ef66d606bee9f54d3
Yamiao Liu <56270840+ymliu978@users.noreply.github.com>
2023-02-10 12:12:56 -0500

    update the link to resume

--- yourData.js
+++ yourData.js
@@ -9,7 +9,7 @@ export default {
     "I'm Yamiao Liu - a web developer and designer based in Ottawa, Canada.",
 
   socials: [
-    { title: "Resume", link: "https://drive.google.com/file/d/1FVaQqrjwWtCPGr1YNSy-aPz7dscGFEMW/view?usp=sharing" },
+    { title: "Resume", link: "https://drive.google.com/file/d/1zLgexcaHi6KTGygd0hJnvHSmpyJT2K-C/view?usp=sharing" },
     { title: "Github", link: "https://github.com/ymliu978" },
     { title: "LinkedIn", link: "https://www.linkedin.com/in/yamiaoliu/" },
     { title: "Instagram", link: "https://www.instagram.com/ymliu978/" },
